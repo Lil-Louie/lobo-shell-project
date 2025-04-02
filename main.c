de467138d38d99ec0083bbd0efb20fd5aca09019
Flacco <Firstclasscleaners00@yahoo.com>
2025-04-02 14:41:10 -0700

    In process steps 1-5

--- main.c
+++ main.c
@@ -16,43 +16,31 @@ int main() {
     char line[MAX_LINE_CHARS];
     // holds separated words based on whitespace
     char* line_words[MAX_LINE_WORDS + 1];
+    // holds the commands seperated based on pipes
+    char* cmds[MAX_LINE_WORDS +1];
 
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
     while( fgets(line, MAX_LINE_CHARS, stdin) ) {
         int num_words = split_cmd_line(line, line_words);
-        int num_cmds = split_pipes(line, line_words);
+        int num_cmds = split_pipes(line, cmds);
 
         if (num_cmds == 1) { // Single command, no pipe
             pid_t pid = fork();
             if (pid == -1) {
                 perror("fork failed");
                 exit(EXIT_FAILURE);
-            }
-            if (pid == 0) {
-                execute_command(line_words[0]);
-            }
-            waitpid(pid, NULL, 0);
-        } else { // Handle piped commands
-            execute_piped_commands(line_words, num_cmds);
-        }
-
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-        if (pid == 0) { 
-            // Child process
+            } 
+            if (pid == 0) { 
             execvp(line_words[0], line_words);
             perror("exec failed");
             exit(EXIT_FAILURE);
-        } else { 
-            // Parent process
+            }  
             int status;
             waitpid(pid, &status, 0);
+            } else { // Handle piped commands
+            execute_piped_commands(cmds, num_cmds);
         }
-
     }
 
     return 0;
--- src/parsetools.c
+++ src/parsetools.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include "constants.h"
 #include "parsetools.h"
+
+int const MAX_LINE = 1024;
+int const MAX_ARGS = 10;
 
 
 
@@ -19,7 +27,7 @@ int split_cmd_line(char* line, char** list_to_populate) {
    while(list_to_populate[i] != NULL && i < MAX_LINE_WORDS - 1)  {
        list_to_populate[++i] = strtok_r(NULL, delimiters, &saveptr);
    };
-
+}
 
 
 
@@ -27,25 +35,50 @@ int split_cmd_line(char* line, char** list_to_populate) {
 // seperated by whitespace
 // Returns the number of commands
 
-int split_pipes(char* line, char**  line_words){
-    char* saveptr;  // for strtok_r; see http://linux.die.net/man/3/strtok_r
-   char* delimiters = " \t\n"; // whitespace
+int split_pipes(char* line, char**  list_of_cmds){
+   char* saveptr;  // for strtok_r; see http://linux.die.net/man/3/strtok_r
+   char* pipes = "|"; // Pipes
    int i = 0;
 
-   list_to_populate[0] = strtok_r(line, delimiters, &saveptr);
+   list_of_cmds[0] = strtok_r(line, pipes, &saveptr);
+   while(list_of_cmds[i] != NULL && i < MAX_LINE_WORDS - 1)  {
+        while (*list_of_cmds[i] == ' ') list_of_cmds[i]++;
+        i++;
+        list_of_cmds[i] = strtok_r(NULL, pipes, &saveptr);
+   }
 
-   while(list_to_populate[i] != NULL && i < MAX_LINE_WORDS - 1)  {
-       list_to_populate[++i] = strtok_r(NULL, delimiters, &saveptr);
+    fprintf(stdout, "Num of cmds (pipe):  %d\n", i);
+    for (int j = 0; j < i; j++) {
+        fprintf(stdout, "command %d: %s\n", j, list_of_cmds[j]);
+    }
 
-       return something;
+    return i;
 }
 
 
 
-
-
 // Executes commands and forks the process
-void execute_piped_commands(char**  line_words, int num_cmds);
+void execute_piped_commands(char**  list_of_cmds, int num_cmds){
+    int pipes[num_cmds - 1][2];
+    pid_t pids[num_cmds];
 
-   return i;
+    // Create pipes
+    for (int i = 0; i < num_cmds - 1; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+        for (int i = 0; i < num_cmds; i++) {
+        pids[i] = fork();
+
+        if (pids[i] == -1) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+         fprintf("The second child's pid is: %d\n", list_of_cmds[i]);
+
+
+        }
 }
